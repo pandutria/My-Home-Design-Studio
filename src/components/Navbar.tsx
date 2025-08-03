49d33792fec031b5941d1756cdb7a3ae2eb07845
pandutria <panduadyatama029@gmail.com>
2025-08-04 02:35:56 +0700

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,14 +37,14 @@ const Navbar = () => {
               <h1 className='font-logo font-semibold text-black lg:text-[15px] md:text-[15px] text-[14px]'>Design Studio</h1>
             </div>
           </div>
-          <div className={`${isOpen ? 'translate-x-0' : 'translate-x-full'} lg:translate-x-0 md:translate-x-0 translate-x-0 transition-transform duration-500 ease-in-out lg:flex md:flex flex lg:relative md:relative absolute top-0 right-0 lg:h-auto md:h-auto h-screen lg:flex-row md:flex-row flex-col lg:gap-7 md:gap-6 gap-5 lg:px-0 md:px-0 px-5 bg-white justify-center items-center`}>
+          <div className={`${isOpen ? 'translate-x-0' : 'translate-x-full'} lg:translate-x-0 md:translate-x-0 transition-transform duration-500 ease-in-out lg:flex md:flex flex lg:relative md:relative absolute top-0 right-0 lg:h-auto md:h-auto h-screen lg:flex-row md:flex-row flex-col lg:gap-7 md:gap-6 gap-5 lg:px-0 md:px-0 px-5 bg-white justify-center items-center`}>
             <a href="#home" className={`${activeSection == 'home' ? 'text-primary font-semibold' : 'text-black font-normal'} font-poppins text-[15px]`}>Beranda</a>
             <a href="#about"className={`${activeSection == 'about' ? 'text-primary font-semibold' : 'text-black font-normal'} font-poppins text-[15px]`}>Tentang</a>
             <a href="#service" className={`${activeSection == 'service' ? 'text-primary font-semibold' : 'text-black font-normal'} font-poppins text-[15px]`}>Layanan</a>
             <a href="#testimonial" className={`${activeSection == 'testimonial' ? 'text-primary font-semibold' : 'text-black font-normal'} font-poppins text-[15px]`}>Testimoni</a>
             <a href="#contact" className='font-poppins font-semibold text-white text-[13.5px] bg-primary rounded-[50px] py-2 px-4 items-center'>Kontak Kami</a>
           </div>
-          <img src={hamburger} onClick={() => setIsOpen(!isOpen)} className={` ${isOpen ? 'rotate-90' : 'rotate-0'} lg:hidden md:hidden flex w-6 h-auto object-contain cursor-pointer transition-transform duration-300 z-50`} alt="" />
+          <img src={hamburger} onClick={() => setIsOpen(!isOpen)} className={` ${isOpen ? 'rotate-90' : 'rotate-0'} lg:hidden md:hidden flex w-6 h-auto object-contain cursor-pointer transition-transform duration-300 z-50 relative`} alt="" />
         </div>
       </section>
     </div>
